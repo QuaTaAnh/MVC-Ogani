f78bf09314d4db9a1a6fd18ec3547c4f6f0a20ef
AnhTran <letrannhatanh171@gmail.com>
2023-02-24 18:33:36 +0700

    sanphamtheoloai

--- WebNo3/Controllers/HomeController.cs
+++ WebNo3/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebNo3.Models;
+using X.PagedList;
 
 namespace WebNo3.Controllers
 {
@@ -14,14 +16,25 @@ namespace WebNo3.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            return View();
-        }
+            //Phân trang
+            int pageSize = 12;
+            int pageNumber = page == null || page <=0 ? 1 : page.Value;
+            var lstSanPham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.MaSp);
 
-        public PartialViewResult LoaiPartial()
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstSanPham, pageNumber, pageSize);
+            return View(lst);
+        }
+        public IActionResult SanPhamTheoLoai(string maloai, int page=1)
         {
-            return PartialView(db.TLoaiSps.ToList());
+            int pageSize = 8;
+            int pageNumber = page;
+            var lstSanPham = db.TDanhMucSps.Where(x => x.MaLoai == maloai).AsNoTracking().OrderBy(x => x.MaSp);
+
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstSanPham, pageNumber, pageSize);
+            ViewBag.maloai = maloai;
+            return View(lst);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
